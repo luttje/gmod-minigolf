4c0bf9cc1e364c272fb80c6895735c8f3c668f30
luttje <2738114+luttje@users.noreply.github.com>
2025-09-07 20:41:27 +0200

    remove old betting command

--- gamemodes/gm_minigolf/gamemode/init.lua
+++ gamemodes/gm_minigolf/gamemode/init.lua
@@ -3,12 +3,9 @@ include("sh_init.lua")
 --[[
 	Commands
 --]]
+
 Minigolf.Commands.Register("team", function(player)
 	net.Start("Minigolf.ShowGolfTeamMenu")
 	net.WriteBool(false)
 	net.Send(player)
 end, "Show information about your Minigolf team, to change it, leave it and/or join another team")
-
-Minigolf.Commands.Register("bet", function(player)
-	player:ConCommand("menu_betting")
-end, "Bet on upcoming players' performance")
